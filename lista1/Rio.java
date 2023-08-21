bdda9698104abe296a8f0d28917d8bc1a074a0b1
LeoSParra <108582759+LeoSParra@users.noreply.github.com>
2023-08-20 23:08:59 -0300

    Add files via upload

---
+++ lista1/Rio.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lista1;
+
+/**
+ *
+ * @author leo
+ */
+public class Rio {
+    public String nome;
+    public float nivel;
+    public boolean poluido;
+    
+    public Rio (String nome, float nivel, boolean poluido){
+        this.nome = nome;
+        this.nivel = nivel;
+        this.poluido = poluido;
+    }
+    
+    
+    
+}
--- lista1/TestaALuno.java
+++ lista1/TestaALuno.java
@@ -3,8 +3,8 @@ package lista1;
 
 public class TestaALuno {
     
-    public static void main(String args[]) {
-        Aluno leo = new Aluno (15,"leo", 19, 10f, 9.5f);
+    public static void main(String[] args) {
+        Aluno leo = new Aluno (15,"leo", 19, 4f, 3f);
         
         leo.notaFinal();
         leo.dadosAluno();
