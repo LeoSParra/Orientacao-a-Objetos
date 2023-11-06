9da9dee6177aa5c25401ab63fe8cc105ed858aa4
LeoSParra <108582759+LeoSParra@users.noreply.github.com>
2023-11-06 12:25:06 -0300

    Add files via upload

---
+++ workspace-spring-tool-suite/bomDia/src/main/java/br/edu/fatecfranca/bomDia/BomDiaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.bomDia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomDiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomDiaApplication.class, args);
+	}
+
+}
---
+++ workspace-spring-tool-suite/bomDia/src/test/java/br/edu/fatecfranca/bomDia/BomDiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fatecfranca.bomDia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomDiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
